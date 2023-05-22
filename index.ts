6b713138065423d25306e79df6aba0aa77dca357
Victor Queiroz <victorcqueirozg@gmail.com>
2023-05-21 21:33:29 -0300

    initial commit
    Signed-off-by: Victor Queiroz <victorcqueirozg@gmail.com>

---
+++ index.ts
@@ -0,0 +1,27 @@
+import child_process from "child_process";
+
+export class Exception {
+  public constructor(public readonly what = "") {}
+}
+
+export function spawn(
+  command: string,
+  args: string[] = [],
+  options: child_process.SpawnOptionsWithoutStdio = {}
+) {
+  const childProcess = child_process.spawn(command, args, {
+    stdio: "inherit",
+    ...options,
+  });
+  return {
+    wait: () =>
+      new Promise<void>((resolve, reject) => {
+        childProcess.on("close", (code) => {
+          if (code !== 0) {
+            reject(new Exception(`Process exited with code: ${code}`));
+          }
+        });
+      }),
+    childProcess,
+  };
+}
